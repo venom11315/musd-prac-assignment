b41680a737070dfea33222099687e00894c86a30
venom11315 <33302124+venom11315@users.noreply.github.com>
2018-11-15 22:00:40 +0800

    NEWEST MUSD ASSIGNMENT
    ASSIGNMENT

--- Movie Rater/app/src/main/java/com/nyp/sit/dit/it2107/movierater/MovieRater.kt
+++ Movie Rater/app/src/main/java/com/nyp/sit/dit/it2107/movierater/MovieRater.kt
@@ -5,9 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.renderscript.Sampler
-import android.view.View;
+import android.view.View
 import android.widget.RadioButton
-import android.widget.Toast;
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_movie_rater.*
 import android.widget.RadioGroup
 import com.nyp.sit.dit.it2107.movierater.R.layout.view_movie
@@ -21,54 +21,75 @@ class MovieRater : AppCompatActivity() {
     }
 
     fun addMovie(v:View?){
-        var title:String = "Title = "+name.text
-        var overview:String = "Overview = "+desc.text
-        var release:String = "Release date = "+relDate.text
+        var title:String = name.text.toString()
+        var overview:String = desc.text.toString()
+        var release:String = relDate.text.toString()
 
         var choice = langChoice.checkedRadioButtonId
         //find selected radio button
         var button = findViewById<RadioButton>(choice)
 
-        var check:String = "";
-        var aud:Boolean;
+        var check:String = ""
+        var aud:Boolean
+        var valid:Boolean = true
+        var suitMr:String
+
+        if(choice < 0){
+            tamil.error = "Select a language"
+            valid = false
+        }
         if(audience.isChecked){
-            aud = true;
+            aud = true
             if(violence.isChecked){
                 check = violence.text.toString()
                 if(lanUsed.isChecked){
                     check = violence.text.toString() + "\n" + lanUsed.text.toString()
                 }
-            }else{
+            }else if(lanUsed.isChecked){
                 check = lanUsed.text.toString()
             }
+            else{
+                audience.error = "Please select violence or language used"
+                valid = false
+            }
+            suitMr = "No"
         }else{
-            aud = false;
+            aud = false
+            suitMr = "Yes"
         }
         if(name.text.toString() == ""){
-            name.setError("Field empty")
+            name.error = "Field empty"
+            valid = false
         }
         if(desc.text.toString() == ""){
-            desc.setError("Field empty")
+            desc.error = "Field empty"
+            valid = false
         }
         if(relDate.text.toString() == ""){
-            relDate.setError("Field empty")
-        }else{
-            var total:String = title+"\n"+overview+"\n"+release+"\n"+"Language = "+button.text+"\n"+"Suitable for all ages = "+aud.toString()+"\n"+"Reason = "+"\n"+check;
-            Toast.makeText(this, total, Toast.LENGTH_LONG).show()
+            relDate.error = "Field empty"
+            valid = false
+        }
+        if(valid == true) {
+            var total: String = "Title= " + title + "\n" + "Overview= " + overview + "\n" + "Release Date= " + release + "\n" + "Language = " + button.text + "\n" + "Suitable for all ages = " + aud.toString() + "\n" + "Reason = " + "\n" + check
+            Toast.makeText(this, total, Toast.LENGTH_SHORT).show()
+            MovieEntity(title, overview, release, button.text.toString(), suitMr)
         }
 
     }
 
     //checkbox onclick event
     fun suitable(v:View?) {
-        fun suitable(v: View?) {
-            if (audience.isChecked) {
-                violence.visibility = View.VISIBLE;
-                lanUsed.visibility = View.VISIBLE;
-            } else {
-                violence.visibility = View.INVISIBLE;
-                lanUsed.visibility = View.INVISIBLE;
-            }
+        if (audience.isChecked) {
+            violence.visibility = View.VISIBLE
+            lanUsed.visibility = View.VISIBLE
+        } else {
+            violence.visibility = View.INVISIBLE
+            lanUsed.visibility = View.INVISIBLE
         }
+    }
+
+    fun movieView(v:View){
+        var viewIntent = Intent(applicationContext, ViewMovie::class.java)
+        startActivity(viewIntent)
     }
 }
--- Movie Rater/app/src/main/java/com/nyp/sit/dit/it2107/movierater/ViewMovie.kt
+++ Movie Rater/app/src/main/java/com/nyp/sit/dit/it2107/movierater/ViewMovie.kt
@@ -1,7 +1,7 @@
 package com.nyp.sit.dit.it2107.movierater
 
 import android.os.Bundle
-import android.view.View;
+import android.view.View
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_movie_rater.*
 
